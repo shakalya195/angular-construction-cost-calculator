e55764870be0023e9fe62ee333652d9faee02463
shakalya195 <shakalya195@gmail.com>
2022-09-07 19:27:57 +0530

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,20 @@ export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
   items: any[] = [
-    { active: true, title: 'CEMENT', qty: 300, price: 450 },
-    { active: true, title: 'STEEL', qty: 2500, price: 45 },
-    { active: true, title: 'SAND', qty: 2000, price: 55 },
-    { active: true, title: 'AGREGATE', qty: 1500, price: 22 },
-    { active: true, title: 'BRICKS', qty: 7, price: 20000 },
+    { active: true, title: 'CEMENT', qty: 400, price: 450 },
+    { active: true, title: 'STEEL', qty: 4000, price: 65 },
+    { active: true, title: 'SAND', qty: 81, price: 1000 },
+    { active: true, title: 'AGREGATE', qty: 60, price: 1000 },
+    { active: true, title: 'BRICKS', qty: 20000, price: 7 },
     { active: true, title: 'LABOUR', qty: 1000, price: 150 },
     { active: true, title: 'VATRIFIED TILES', qty: 600, price: 35 },
     { active: true, title: 'CEREMIC TILES', qty: 300, price: 20 },
     { active: true, title: 'GRANITE', qty: 20, price: 150 },
     { active: true, title: 'KOTA STONE', qty: 250, price: 40 },
     { active: true, title: 'DOOR FRAME', qty: 8, price: 1800 },
-    { active: true, title: 'DOOR 1', qty: 5, price: 3500 },
-    { active: true, title: 'DOOR 2', qty: 3, price: 2500 },
+    { active: true, title: 'Main Door 6ft', qty: 1, price: 8000 },
+    { active: true, title: 'DOOR 3ft', qty: 3, price: 3500 },
+    { active: true, title: 'DOOR 2.5ft', qty: 1, price: 2500 },
     { active: true, title: 'WINDOW GRILL', qty: 4, price: 1800 },
     { active: true, title: 'SLIDING WINDOW', qty: 4, price: 2500 },
     { active: true, title: 'WATER TANK', qty: 1000, price: 6 },
@@ -29,7 +30,7 @@ export class AppComponent {
     { active: true, title: 'PLUMBING', qty: 1, price: 30000 },
     { active: true, title: 'ELECTRIFICATION', qty: 1, price: 30000 },
     { active: true, title: 'EXCAVATION AND FILLING', qty: 1, price: 30000 },
-    { active: true, title: 'CURING', qty: 1, price: 30000 },
+    { active: false, title: 'CURING', qty: 1, price: 30000 },
     { active: true, title: 'EXTRA', qty: 1, price: 50000 },
   ];
 
